fe5b68b88c68bfeafff9486ccac2a28d741455ac
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-09-09 13:13:51 +0300

    Add files via upload

--- Async.js
+++ Async.js
@@ -1,14 +1,23 @@
 
 
-const xhr = new XMLHttpRequest();
-xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
-xhr.addEventListener("load", () => {
-    console.log(xhr.responseText);
-});
 
-xhr.addEventListener("error", () => {
-    console.error('Error');
-})
+function getPosts(cb){ 
+     const xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+    xhr.addEventListener("load", () => {
+        const response = JSON.parse(xhr.responseText)
+        cb(response);
+    });
+
+    xhr.addEventListener("error", () => {
+        console.error('Error');
+    })
 
 xhr.send();
+}
+
+getPosts((response) => {
+    console.log(response);
+});
 
