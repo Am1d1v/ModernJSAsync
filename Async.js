1803c9737d37d06f3f4c1e759bea2de373b2dcd1
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-09-09 13:24:47 +0300

    Add files via upload

--- Async.js
+++ Async.js
@@ -1,5 +1,29 @@
 
 
+const btn = document.querySelector('button');
+
+btn.addEventListener("click",() => {
+    getPosts((response) => {
+        //console.log(response);
+        const fragment = document.createDocumentFragment();
+        response.forEach((post) => {
+            const card = document.createElement('div');
+            card.classList.add('card');
+            const cardBody = document.createElement('div');
+            cardBody.classList.add('card-body');
+            const title = document.createElement('h5');
+            title.classList.add('card-title');
+            title.textContent = post.title;
+            const article = document.createElement('p');
+            article.classList.add('card-text');
+            article.textContent = post.body;
+            cardBody.appendChild(title);
+            cardBody.appendChild(article);
+            console.log(cardBody);
+        })
+    });
+    
+})
 
 function getPosts(cb){ 
      const xhr = new XMLHttpRequest();
@@ -17,7 +41,4 @@ function getPosts(cb){
 xhr.send();
 }
 
-getPosts((response) => {
-    console.log(response);
-});
 
