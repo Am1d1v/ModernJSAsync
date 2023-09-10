d219b2a314fd149cd9ccd79105e63fe29dc1132f
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-09-10 19:03:56 +0300

    Add files via upload

--- Promise.js
+++ Promise.js
@@ -60,7 +60,11 @@ getPost2(3)
     */
 
 async function getPost(id){
+
+    
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+
+   // throw new Error('Error');
 
     const data = await response.json();
     return data;
