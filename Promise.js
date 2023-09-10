1cad0d9624960da24e493ac540c7cf0b8b27eff1
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-09-10 18:25:26 +0300

    Add files via upload

--- Promise.js
+++ Promise.js
@@ -30,14 +30,52 @@ fetch('https://jsonplaceholder.typicode.com/posts')
 })
 */
 
+/*
 function getPost(id){
     return new Promise((resolve, reject) =>{
         fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
         .then(response => response.json())
         .then(post => resolve(post))
-        .catch(error => console.log(error))
+        .catch(error => reject(error))
     });
 }
 
 getPost(1)
     .then(post => console.log(post));
+*/
+
+function getPost2(id){
+    return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then(response => response.json())
+}
+
+getPost2(2)
+    .then(post => console.log(post))
+    .catch(error => console.log(error));
+
+getPost2(3)
+    .then(post => console.log(post))
+    .catch(error => console.log(error));    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
